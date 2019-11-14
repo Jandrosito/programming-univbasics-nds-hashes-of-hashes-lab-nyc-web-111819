f9ba8cdb1287f476bcd74ba28e2557eb20137307
Edward Castillo <castilloedwarda@gmail.com>
2019-11-14 15:01:17 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,12 +43,12 @@ def naming_system
   # HoH all the way to Species!
   {
  {label: "Kingdom",
-  sub_category: {label: "Phylum",sub_category: {label: "Class"}},
+  sub_category: {label: "Phylum",sub_category: {label: "Class"}}
   
-  {label: "Order" },
+  {label: "Order" }
  
  {label: "Family",
-   sub_category: {label: "Genus",}},
+   sub_category: {label: "Genus",}}
  
   { label: "Species",
    sub_category: nil}
